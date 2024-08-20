c6f850c7de3e12c986263619604289f1f9be1adb
Sanket Shetty <legendhimself007@gmail.com>
2024-08-20 18:44:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//   Footer Carousel
+
+const carouselItems = document.querySelectorAll(".carousel-item");
+const prevButton = document.getElementById("prev-btn");
+const nextButton = document.getElementById("next-btn");
+let currentSlide = 0;
+
+function updateCarousel() {
+  for (let i = 0; i < carouselItems.length; i++) {
+    carouselItems[i].classList.remove("active");
+  }
+  carouselItems[currentSlide].classList.add("active");
+}
+
+prevButton.addEventListener("click", () => {
+  currentSlide =
+    (currentSlide - 1 + carouselItems.length) % carouselItems.length;
+  updateCarousel();
+});
+
+nextButton.addEventListener("click", () => {
+  currentSlide = (currentSlide + 1) % carouselItems.length;
+  updateCarousel();
+});
+
+updateCarousel();
+
+// Auto Type Title
+
+let typed = new Typed(".auto-typers", {
+  strings: ["SMB revolution!"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+
+// Dark Mode Jquery
+
+$(document).ready(function () {
+  $(".darkmoding").click(function () {
+    $("body").toggleClass("darkModer");
+    $(".darkmoding").toggleClass("darkModer2");
+    $(".nav-li-a").toggleClass("nav-li-a-dark");
+    $(".btn-outline-dark").toggleClass("btn-outline-moder");
+    $(".btn-dark2").toggleClass("btn-outline-moder2");
+    $(".btn-waitlist").toggleClass("btn-waitlist-dark");
+  });
+});
